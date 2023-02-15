19512656cbf1fd6135b2bcb887d1465d0c6d66f6
Juan Aguirre <116675833+AnubisGP@users.noreply.github.com>
2023-02-15 15:13:30 -0500

    Create miPrimerPrograma.java

---
+++ miPrimerPrograma.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class miPrimerPrograma {
+    public static void main (String []args){
+        Scanner entrada = new Scanner (System.in);
+        int n;
+        System.out.println("Ingrese un numero: ");
+        n = entrada.nextInt();
+        System.out.println("El numero almacenado es: "+n);
+
+    }
+
+}
