383e891f7efffa3f22031ba22b06f9043524c2f7
Juan Aguirre <116675833+AnubisGP@users.noreply.github.com>
2023-03-22 14:57:40 -0500

    Create del1al19.java

---
+++ del1al19.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main{
+    public static void main (String[] args) {
+        Scanner m = new Scanner(System.in);
+        int x;
+        double y;
+        y=(int) (Math.random()*19);
+        for (int i=1; i<=5; i++){
+            System.out.println("Intento numero "+i+" para adivinar un numero del 1 al 19");
+            x = m.nextInt();
+          if (x>=0 && x<20){
+                if(x==y){
+                    System.out.println("¡Felicidades, Has adivinado el numero!");
+                    break;
+                }
+                else{
+                    System.out.println("No has logrado adivinar el numero "+y);
+                }
+          }else
+            System.out.println("Valor incorrecto");
+            i--;
+        }
+    }
+}
